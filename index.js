cf9fb077f1cd5afd99213a82d8acc4489c30141c
Rutik Gagare <rutikgagare091971@gmail.com>
2022-03-01 16:54:19 +0530

    searchicon

--- index.js
+++ index.js
@@ -4,5 +4,13 @@ function displaysearch(){
 
 function displaynav(){
     document.getElementById('mobilenav').style.display = "inline";
+    document.getElementById('cross').style.display = "inline";
+    document.getElementById('ham').style.display = "none";
+}
+
+function closenav(){
+    document.getElementById('mobilenav').style.display = "none";
+    document.getElementById('cross').style.display = "none";
+    document.getElementById('ham').style.display = "inline";
 }
 
